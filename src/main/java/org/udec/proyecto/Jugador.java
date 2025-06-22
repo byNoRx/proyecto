d4efd1b9ccaf8b5aff9f5344fbf508f9e50f423c
byNoRx <norxgamer12@gmail.com>
2025-06-22 16:34:54 -0400

    Añadir clase Jugador que representa un jugador individual que es un participante con dirección

---
+++ src/main/java/org/udec/proyecto/Jugador.java
@@ -0,0 +1,18 @@
+package org.udec.proyecto;
+
+public class Jugador extends Participante {
+    private String direccion;
+    
+    public Jugador(String nombre, String email, String telefono, String direccion) {
+        super(nombre, email, telefono); 
+        this.direccion = direccion;
+    }
+    
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}

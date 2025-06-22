221e0c8d3cfef9c7ea9f85a91562ce410f901f36
byNoRx <norxgamer12@gmail.com>
2025-06-22 16:35:53 -0400

    Añadir clase Equipo que representa un equipo que es participante y puede tener jugadores

---
+++ src/main/java/org/udec/proyecto/Equipo.java
@@ -0,0 +1,41 @@
+package org.udec.proyecto;
+
+import java.util.ArrayList;
+
+public class Equipo extends Participante {
+    private ArrayList<Jugador> jugadores;
+    
+    public Equipo(String nombre, String email, String telefono) {
+        super(nombre, email, telefono); 
+        jugadores = new ArrayList<>();
+    }
+
+
+    /**
+     * Añade un jugador al equipo
+     *
+     * @param jugador el jugador que sera añadido
+     */
+    public void AddJugador(Jugador jugador) {
+        jugadores.add(jugador);
+    }
+
+    /**
+     * Elimina un jugador del equipo
+     *
+     * @param jugador el jugador que sera eliminado
+     */
+    public void RemoveJugador(Jugador jugador) {
+        jugadores.remove(jugador);
+    }
+
+    /**
+     * Devuelve la lista de jugadores
+     *
+     * @return la lista de jugadores
+     */
+    public ArrayList<Jugador> getJugadores() {
+        return jugadores;
+    }
+
+}

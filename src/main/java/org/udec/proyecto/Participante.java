b72fda292244ea468b95be1cca22e41689dd28f9
byNoRx <norxgamer12@gmail.com>
2025-06-22 16:32:59 -0400

    Añadir clase abstracta Participante que representa un participante con nombre, e-mail y teléfono.

---
+++ src/main/java/org/udec/proyecto/Participante.java
@@ -0,0 +1,37 @@
+package org.udec.proyecto;
+
+public abstract class Participante {
+    private String nombre;
+    private String email;
+    private String telefono;
+    
+    public Participante(String nombre, String email, String telefono) {
+        this.nombre = nombre;
+        this.email = email;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}

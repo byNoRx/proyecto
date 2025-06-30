6f5803faf20032614f1dfd922de0af1c16d1393c
byNoRx <norxgamer12@gmail.com>
2025-06-29 20:23:29 -0400

    Añadir atributo para almacenar el formato del torneo y sus métodos getter y setter..

--- src/main/java/org/udec/proyecto/Torneo.java
+++ src/main/java/org/udec/proyecto/Torneo.java
@@ -1,15 +1,16 @@
 package org.udec.proyecto;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Torneo {
     private String nombre;
     private String disciplina;
+    private FormatoTorneo formato;
     private ArrayList<Participante> participantes;
 
-    public Torneo(String nombre, String disciplina) {
+    public Torneo(String nombre, String disciplina, FormatoTorneo formato) {
         this.nombre = nombre;
         this.disciplina = disciplina;
+        this.formato = formato;
         this.participantes = new ArrayList<>();
     }
 
@@ -27,6 +28,14 @@ public class Torneo {
 
     public void setDisciplina(String disciplina) {
         this.disciplina = disciplina;
+    }
+
+    public FormatoTorneo getFormato() {
+        return formato;
+    }
+
+    public void setFormato(FormatoTorneo formato) {
+        this.formato = formato;
     }
 
     /**

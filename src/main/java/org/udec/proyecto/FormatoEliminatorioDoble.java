4c9727211d86572d4dba739791c42ffcdd9c4f9b
byNoRx <norxgamer12@gmail.com>
2025-06-29 21:18:29 -0400

    Añadir formatos de torneos que implementan la interfaz FormatoToreno.

---
+++ src/main/java/org/udec/proyecto/FormatoEliminatorioDoble.java
@@ -0,0 +1,9 @@
+package org.udec.proyecto;
+import java.util.ArrayList;
+
+public class FormatoEliminatorioDoble implements FormatoTorneo {
+    @Override
+    public void generarPartidos(ArrayList<Participante> participantes) {
+        // TODO: Implementar la generación de la eliminatoria doble.
+    }
+}

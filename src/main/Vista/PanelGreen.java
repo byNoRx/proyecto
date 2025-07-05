40a12f9e56efd8b091fff468edb029436e945526
ignacio121234 <igarcia2020@udec.cl>
2025-07-05 16:56:15 -0400

    creacion base de la interfaz del app

---
+++ src/main/Vista/PanelGreen.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelGreen extends JPanel {
+    public PanelGreen() {
+        setLayout(new BorderLayout());
+        setPreferredSize(new Dimension(600, 600));
+        setBackground(Color.GREEN);
+
+
+    }
+}

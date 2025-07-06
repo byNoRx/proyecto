254dff9397b96b53e0fd06200a7343b7ba08797a
byNoRx <norxgamer12@gmail.com>
2025-07-06 19:20:22 -0400

    Añadir métodos para ordenar participantes

--- src/main/java/org/udec/proyecto/FormatoEliminatorioDoble.java
+++ src/main/java/org/udec/proyecto/FormatoEliminatorioDoble.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 
 public class FormatoEliminatorioDoble implements FormatoTorneo {
     @Override
+    public void ordenarParticipantes(ArrayList<Participante> participantes) {
+        // TODO: Implementar el ordenamiento de la eliminatoria doble.
+    }
+    
+    @Override
     public void generarPartidos(ArrayList<Participante> participantes, ArrayList<Partido> partidos, LocalDate fechaDeInicio, int diasEntreRondas) {
         // TODO: Implementar la generación de la eliminatoria doble.
     }
--- src/main/java/org/udec/proyecto/FormatoLigaSimple.java
+++ src/main/java/org/udec/proyecto/FormatoLigaSimple.java
@@ -5,6 +5,11 @@ import java.time.LocalDate;
 
 public class FormatoLigaSimple implements FormatoTorneo {
     @Override
+    public void ordenarParticipantes(ArrayList<Participante> participantes) {
+        // TODO: Implementar el ordenamiento de la liga simple.
+    }
+
+    @Override
     public void generarPartidos(ArrayList<Participante> participantes, ArrayList<Partido> partidos, LocalDate fechaDeInicio, int diasEntreRondas) {
         // TODO: Implementar la generación de la liga simple.
     }

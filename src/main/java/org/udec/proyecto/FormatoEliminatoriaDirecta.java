ea7f8a2f70544c63520d816ac8e12beca60685af
byNoRx <norxgamer12@gmail.com>
2025-07-06 17:13:06 -0400

    Eliminar print y espacios sin utilidad.

--- src/main/java/org/udec/proyecto/FormatoEliminatoriaDirecta.java
+++ src/main/java/org/udec/proyecto/FormatoEliminatoriaDirecta.java
@@ -6,13 +6,10 @@ import java.time.LocalDate;
 public class FormatoEliminatoriaDirecta implements FormatoTorneo {
     @Override
     public void generarPartidos(ArrayList<Participante> participantes, ArrayList<Partido> partidos, LocalDate fechaDeInicio, int diasEntreRondas) {
-
         int numParticipantes = participantes.size();
         int numPartidos = numParticipantes / 2;
         int numParticipantesSobrantes = numParticipantes % 2;
         LocalDate fechaPartido = fechaDeInicio;
-
-        System.out.println("Formato Eliminatoria Directa");
         while (numPartidos > 0) {
             for (int i = 0; i < numPartidos; i++) {
                 partidos.add(new Partido(fechaPartido));

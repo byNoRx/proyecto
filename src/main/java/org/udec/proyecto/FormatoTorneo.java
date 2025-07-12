14326345d332ae7d3ba91c099f719eb877adbd3e
NoRx <norxgamer12@gmail.com>
2025-07-11 22:38:51 -0400

    Añadir método conectarPartidos a FormatoTorneo para hacer los brackets del torneo

--- src/main/java/org/udec/proyecto/FormatoTorneo.java
+++ src/main/java/org/udec/proyecto/FormatoTorneo.java
@@ -6,4 +6,5 @@ import java.time.LocalDate;
 public interface FormatoTorneo {
     public void ordenarParticipantes(ArrayList<Participante> participantes);
     public void generarPartidos(ArrayList<Participante> participantes, ArrayList<Partido> partidos, LocalDate fechaDeInicio, int diasEntreRondas) throws ParticipantesInsuficientesException;
+    public void conectarPartidos(ArrayList<Partido> partidos);
 }
